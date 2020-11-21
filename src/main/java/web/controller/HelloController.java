e95de88385f1c0af8cc1eb51d0beae36368cf46b
aiglazunov <dr_west@mail.ru>
2020-11-21 22:02:54 +0300

    add calculator

--- src/main/java/web/controller/HelloController.java
+++ src/main/java/web/controller/HelloController.java
@@ -1,9 +1,11 @@
 package web.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -19,6 +21,33 @@ public class HelloController {
 		messages.add("5.2.0 version by sep'19 ");
 		model.addAttribute("messages", messages);
 		return "index";
+	}
+
+	@GetMapping(value = "/calculator")
+	public String calculator(@RequestParam("a") int a, @RequestParam("b") int b	,
+							 @RequestParam("action") String action, Model model) {
+		double result;
+
+		switch (action) {
+			case "multiply":
+				result = a * b;
+				break;
+			case "divide":
+				result = a / (double) b;
+				break;
+			case "minus":
+				result = a - b;
+				break;
+			case "add":
+				result = a + b;
+				break;
+			default:
+				result = -1;
+				break;
+		}
+
+		model.addAttribute("result", result);
+		return "/calculator";
 	}
 	
 }
